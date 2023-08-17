1fad20ebf1541be124c92e97eb9ec41fe4644d7b
Roshan Kumar Singh <roshankumarsingh9876@gmail.com>
2023-08-18 00:19:21 +0530

    adding upadate read and deleting

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -11,7 +11,7 @@ CreateBook = async(req,res) => {
         newBook.category = req.body.category;
 
         const saveBook = await newBook.save();
-        console.log(saveBook);
+        // console.log(saveBook);
         res.status(200).json(
             {
              message: 'Book saved successfully',
@@ -25,6 +25,59 @@ CreateBook = async(req,res) => {
         res.status(500).json({error: 'Error saving book'});
     }
 
+}
+//get all data 
+GetAllBookDetails = async  (req, res) =>{
+    try {
+        const books = await Book.find({});
+        console.log(books);
+        res.json(books);
+    } catch (err) {
+        console.log('Error:', err);
+        res.status(500).send('an error occured')
+    }
+}
+//update book details
+UpdateData = async (req, res) =>{
+
+    try {
+        const updatebook = await Book.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: {
+                    title: req.body.title,
+                    author : req.body.author ,
+                    category: req.body.category
+                }
+            },
+            {new: true});
+             
+        if(!updatebook){
+            res.status(404).send('Book not found');
+        }else{
+            res.send(updatebook);
+        }
+
+    } catch (err) {
+        console.log('Error:', err);
+        res.status(500).send('an error occured');
+    }
+}
+
+//deleting book
+DeletingBook = async (req, res) => {
+
+    try {
+        const book = await Book.findByIdAndDelete(req.params.id);
+        if(!book){
+            res.status(404).send('Book not found');
+        }else{
+            res.send(book);
+        }
+    } catch (err) {
+        console.log('Error:', err);
+        res.status(500).send('an error occured');
+    }
 }
 
 
--- routes/book.route.js
+++ routes/book.route.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router =express();
-const BookController = require('../controllers/book.controller');
+const BookController = require('../controllers/book.controller.js');
 
 router.post("/addBook",BookController.CreateBook);
+
+ router.get("/getAllData",BookController.GetAllBookDetails);
+
+ router.put("/:id",BookController.UpdateData);
+
+ router.delete("/:id",BookController.DeletingBook);
 
 module.exports = router;
